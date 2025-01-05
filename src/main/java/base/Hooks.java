4c816692d23510a011c8101a34c694f8b12140d8
SUDHIR MALIK <sudhirmalik8051@gmail.com>
2025-01-05 14:17:08 +0530

    Project moved to repo

--- src/main/java/base/Hooks.java
+++ src/main/java/base/Hooks.java
@@ -2,12 +2,17 @@ package base;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -39,8 +44,16 @@ public class Hooks extends Base{
 		}
 		driver.get(prop.getProperty("url"));
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(40));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+		WebElement name=driver.findElement(By.xpath("//input[@autocomplete='email']"));
+		WebDriverWait wait= new  WebDriverWait((WebDriver) driver, Duration.ofSeconds(15));
+		wait.until(ExpectedConditions.visibilityOf(name));
+		name.sendKeys(prop.getProperty("Name"));
+		WebElement pass = driver.findElement(By.xpath("//input[@autocomplete='current-password']"));
+		pass.sendKeys(prop.getProperty("Password"));
+		WebElement click = driver.findElement(By.xpath("//input[@value='Log in']"));
+		click.click();
 
 	}
 	
